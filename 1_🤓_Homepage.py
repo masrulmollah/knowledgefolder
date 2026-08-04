8a5435673c0fc6b404548665b143041225a18152
masrulmollah <54163010+masrulmollah@users.noreply.github.com>
2026-08-04 11:39:19 +0600

    Add files via upload

--- 1_🤓_Homepage.py
+++ 1_🤓_Homepage.py
@@ -117,7 +117,6 @@ BUCKETS = {
             "Economics":         "🌐",
             "Leadership":        "🧭",
             "Business Strategy": "♟️",
-            "Home":              "🏠",
         },
     },
 }
